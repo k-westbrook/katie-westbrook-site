56eda95d725a24bbd91f29e116eb72364faa92e0
k-westbrook <kcheriewestbrook@gmail.com>
2019-04-03 12:59:07 -0700

    updated font;

--- src/TitlePage.js
+++ src/TitlePage.js
@@ -8,7 +8,7 @@ class TitlePage extends Component {
       <div className="App">
         <header className="App-header">
           <h2 className='title-header'>Katie Westbrook</h2>
-          <img className='profile-pic' src='https://res.cloudinary.com/dmp2crnzz/image/upload/v1554309949/personal-site/profile_pic.jpg' alt="Katie Westbrook" />
+
         </header>
 
       </div>
